92f7fae2da8dccc3723b6a84e2318867eda7c425
Renat Mirzoev <renat.mirzoev.00@gmail.com>
2023-11-20 22:35:19 +0200

    Fix jenkins

--- cloud/jenkins-helm.tf
+++ cloud/jenkins-helm.tf
@@ -1,5 +1,11 @@
-resource "helm_release" "nginx" {
-  name       = "nginx"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx"
+resource "helm_release" "jenkins" {
+  name       = "jenkins"
+  repository = "https://charts.jenkins.io"
+  chart      = "jenkins"
+  version    = "4.8.3"
+  namespace  = kubernetes_namespace.jenkins_namespace.metadata[0].name
+
+  values = [
+    file("jenkins/jenkins-helm-values.yaml")
+  ]
 }
