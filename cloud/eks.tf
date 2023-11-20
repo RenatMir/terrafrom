9f524e1d6c340c6325485d084d3077a6a21bfbd7
Renat Mirzoev <renat.mirzoev.00@gmail.com>
2023-11-20 20:17:52 +0200

    Fix config

--- cloud/eks.tf
+++ cloud/eks.tf
@@ -25,9 +25,9 @@ module "eks" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = "temp"
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = "temp"
 }
