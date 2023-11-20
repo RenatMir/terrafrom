3f7c600861d5f4a805f8f1d2bc9a253869c112bc
Renat Mirzoev <renat.mirzoev.00@gmail.com>
2023-11-20 21:54:49 +0200

    Added helm provider

--- cloud/providers.tf
+++ cloud/providers.tf
@@ -19,15 +19,22 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws-iam-authenticator"
-    # This requires the aws-iam-authenticator to be installed locally where Terraform is executed
-    args        = ["token", "--cluster-id", module.eks.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
   }
 }
 
-#provider "helm" {
-#  kubernetes {}
-#}
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
+  }
+}
 
 provider "aws" {
   region = var.aws_region
