0e0efd4d39e61f9795ca0dbee3758dcafb10af03
Renat Mirzoev <renat.mirzoev.00@gmail.com>
2023-11-20 19:30:01 +0200

    Fix config

--- cloud/eks.tf
+++ cloud/eks.tf
@@ -6,14 +6,10 @@ module "eks" {
   cluster_version = var.cluster_version
 
   vpc_id  = module.vpc.vpc_id
-  subnets = module.vpc.private_subnets
+  subnet_ids = module.vpc.private_subnets
 
   tags = {
     Name = "Jenkins-EKS"
-  }
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
   }
 
   eks_managed_node_groups = {
