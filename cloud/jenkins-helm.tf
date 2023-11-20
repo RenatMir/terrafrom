2ea0b8aa93736398f8880bef4de8c615610e05f3
Renat Mirzoev <renat.mirzoev.00@gmail.com>
2023-11-20 22:28:21 +0200

    Fix jenkins repo

--- cloud/jenkins-helm.tf
+++ cloud/jenkins-helm.tf
@@ -1,26 +1,5 @@
-resource "helm_release" "jenkins" {
-  name = "jenkins"
-  chart = "jenkins"
-  repository = "https://github.com/jenkinsci/helm-charts/tree/main/charts/jenkins"
-  namespace = kubernetes_namespace.jenkins_namespace.metadata[0].name
-
-  values = [
-    file("jenkins/jenkins-helm-values.yaml")
-  ]
+resource "helm_release" "nginx" {
+  name       = "nginx"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx"
 }
-
-#resource "kubernetes_config_map" "jcasc" {
-#  metadata {
-#    name = "jcasc"
-#    labels = {
-#      jenkins-jenkins-config = "true"
-#    }
-#    namespace = "jenkins"
-#  }
-#
-#  data = {
-#    "main.yaml" = file("jcasc/main.yaml")
-#    "kubernetes.yaml" = file("jcasc/kubernetes.yaml"),
-#    "pipeline.yaml" = file("jcasc/job.yaml")
-#  }
-#}
