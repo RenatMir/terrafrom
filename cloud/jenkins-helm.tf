74dca9376b08f34b213940f85bdab0200720192e
Renat Mirzoev <renat.mirzoev.00@gmail.com>
2023-11-20 22:55:13 +0200

    Fix jenkins

--- cloud/jenkins-helm.tf
+++ cloud/jenkins-helm.tf
@@ -1,11 +1,17 @@
 resource "helm_release" "jenkins" {
+  provider = helm.primary
+
   name       = "jenkins"
   repository = "https://charts.jenkins.io"
   chart      = "jenkins"
   version    = "3.6.0"
-  namespace  = kubernetes_namespace.jenkins_namespace.metadata[0].name
-
+  namespace  = "jenkins"
+  timeout    = 600
   values = [
     file("jenkins/jenkins-helm-values.yaml")
+  ]
+
+  depends_on = [
+    kubernetes_namespace.jenkins_namespace,
   ]
 }
