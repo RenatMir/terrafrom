bba3dca458004da763fa3e9c2209e94705254df4
Renat Mirzoev <renat.mirzoev.00@gmail.com>
2023-11-20 22:40:55 +0200

    Fix jenkins

--- cloud/jenkins-helm.tf
+++ cloud/jenkins-helm.tf
@@ -2,7 +2,7 @@ resource "helm_release" "jenkins" {
   name       = "jenkins"
   repository = "https://charts.jenkins.io"
   chart      = "jenkins"
-  version    = "4.8.3"
+  version    = "3.6.0"
   namespace  = kubernetes_namespace.jenkins_namespace.metadata[0].name
 
   values = [
