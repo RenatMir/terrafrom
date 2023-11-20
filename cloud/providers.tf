dab84c274dd77f587e5b71e6ef8d062dedddc65c
Renat Mirzoev <renat.mirzoev.00@gmail.com>
2023-11-20 20:27:26 +0200

    Fix config

--- cloud/providers.tf
+++ cloud/providers.tf
@@ -16,7 +16,7 @@ terraform {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64encode(module.eks.cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws-iam-authenticator"
